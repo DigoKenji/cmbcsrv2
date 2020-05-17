4e1a6e840285cb1742a40a97ffde73e264addf4f
PauloHerculano <paulosz@icloud.com>
2020-05-17 20:55:17 -0300

    Update cumbucaserver.js

--- cumbucaserver.js
+++ cumbucaserver.js
@@ -3,17 +3,17 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 var express = require('express');
 var app = express();
 var content;
-​
+
 app.get('/nlu', async function (req, res) {
   content = req.query.text;
   var result = await analyzer(content);
   res.send(result);
 });
-​
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-​
+
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2019-07-12',
   authenticator: new IamAuthenticator({
@@ -21,7 +21,7 @@ const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   }),
   url: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/699e499c-020f-441c-8fc9-693b902b0325',
 });
-​
+
 async function analyzer(content){
 var cont = content;
 var res = "Erro";
@@ -31,15 +31,15 @@ const analyzeParams = {
   },
   'text': cont
 };
-​
+
 await naturalLanguageUnderstanding.analyze(analyzeParams)
   .then(analysisResults => {
-​
+
     res = JSON.stringify(analysisResults, null, 2);
-​
+
   })
   .catch(err => {
-​
+
     console.log('error:', err);
   });
 return res;
